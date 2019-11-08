e70397dfb9df54f76d59ca6629eb241ee32d8abb
zam <ajmzamora@gmail.com>
2019-11-08 15:13:26 -0500

    feat(textparticles): add, style and configure text particles and canvas

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import '../styles/index.sass';
+
+import textParticles from './lib/text-particles.js';
+
+textParticles.accelerate({
+  textList: "BRANDING, LABEL DESIGN, PACKAGING DESIGN, ADVERTISING, LOGO DESIGN, PROMOTIONAL PRODUCTS, MOCKUP, WEB DESIGN, MOTION GRAPHICS, 3D MODEL, CLOTHING, GRAPHICS DESIGN, ART & ILLUSTRATION, MERCHANDIZE, PROOFING",
+  textSize: 26,
+  textColor: '#f6f5f4',
+  particleColor: '#bdd748',
+  particleSize: 3,
+  speed: 1.4,
+});
