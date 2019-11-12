3ad519ace0fbc8b347fe066a7ff0bee36e006981
zam <ajmzamora@gmail.com>
2019-11-11 22:16:17 -0500

    feat(pages): only show pages after click event (enter)

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,12 @@ const svgEnter = document.getElementsByClassName('svg__enter')[0];
 const central = document.getElementById('central');
 const gridPanelLeft = document.getElementsByClassName('grid__panel--left')[0];
 const gridPanelRight = document.getElementsByClassName('grid__panel--right')[0];
+const gridPanelUnderneath = document.getElementsByClassName('grid__underneath')[0];
 
 function enterAnimate() {
   gridPanelLeft.classList.add('grid__panel--leftClicked');
   gridPanelRight.classList.add('grid__panel--rightClicked');
+  gridPanelUnderneath.style.display = 'flex';
   svgEnter.classList.add('svg__enter--clicked');
 }
 
@@ -19,14 +21,13 @@ central.addEventListener('click', ()=> {
     svgEnter.remove();
     gridPanelLeft.remove();
     gridPanelRight.remove();
+    textParticles.accelerate({
+      textList: "BRANDING, LABEL DESIGN, PACKAGING DESIGN, ADVERTISING, LOGO DESIGN, PROMOTIONAL PRODUCTS, MOCKUP, WEB DESIGN, MOTION GRAPHICS, 3D MODEL, CLOTHING, GRAPHICS DESIGN, ART & ILLUSTRATION, MERCHANDIZE, PROOFING",
+      textSize: 18,
+      textColor: '#f6f5f4',
+      particleColor: '#bdd748',
+      particleSize: 3,
+      speed: 0.8,
+    });
   }, 1100);
 }, {once: true});
-
-textParticles.accelerate({
-  textList: "BRANDING, LABEL DESIGN, PACKAGING DESIGN, ADVERTISING, LOGO DESIGN, PROMOTIONAL PRODUCTS, MOCKUP, WEB DESIGN, MOTION GRAPHICS, 3D MODEL, CLOTHING, GRAPHICS DESIGN, ART & ILLUSTRATION, MERCHANDIZE, PROOFING",
-  textSize: 18,
-  textColor: '#f6f5f4',
-  particleColor: '#bdd748',
-  particleSize: 3,
-  speed: 0.8,
-});
